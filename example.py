af93232278eba96bd93b6a9c71a19775cd1c0f36
Oleksandr Polieno (nanvel) <polyenoom@gmail.com>
2018-05-14 20:03:00 +0700

    fix charts template

--- example.py
+++ example.py
@@ -9,18 +9,37 @@ from aiohttp_charts.line import TimeLineChart
 def handler(request):
     composer = ChartsComposer(title="Example")
 
-    chart = TimeLineChart(title='Example chart 1')
+    chart1 = TimeLineChart(title='Example chart 1')
     now = datetime.datetime.utcnow()
-    chart.add_line(
+    chart1.add_line(
         label='Line 1',
         points=[(now + datetime.timedelta(seconds=i), i) for i in range(10)]
     )
-    chart.add_line(
+    chart1.add_line(
         label='Line 2',
         axes_id='axes1',
         points=[(now + datetime.timedelta(seconds=i), 100 - i * 5) for i in range(10)]
     )
-    composer.add_chart(chart)
+    chart1.add_line(
+        label='Line 3',
+        axes_id='axes2',
+        fill=True,
+        points=[(now + datetime.timedelta(seconds=i), 20 + i * 2) for i in range(20)]
+    )
+    composer.add_chart(chart1)
+
+    chart2 = TimeLineChart(title='Example chart 2')
+    now = datetime.datetime.utcnow()
+    chart2.add_line(
+        label='Line 1',
+        points=[(now + datetime.timedelta(seconds=i), i) for i in range(10)]
+    )
+    chart2.add_line(
+        label='Line 2',
+        axes_id='axes1',
+        points=[(now + datetime.timedelta(seconds=i), 10 - i * 5) for i in range(10)]
+    )
+    composer.add_chart(chart2)
 
     return composer.compose(app=request.app)
 
